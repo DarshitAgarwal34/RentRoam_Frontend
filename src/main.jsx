0d3fcb94553ab233ce404366c1021a6712eef13d
DarshitAgarwal34 <agarwaldarshit680@gmail.com>
2026-02-07 15:11:05 +0530

    Deploying at vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <App />
       <Analytics />
-      <SpeedInsights />
     </AuthProvider>
   </React.StrictMode>
 );
